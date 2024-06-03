c6b8d6f7323ebd9bc19f086dafe4991d025a9ce7
Yangfan <chenyangfan3211@gmail.com>
2024-06-03 19:07:09 +0800

    🎨 Preliminary implement createSuperfluidStream

--- packages/nextjs/components/GithubShow.tsx
+++ packages/nextjs/components/GithubShow.tsx
@@ -1,16 +1,36 @@
+import { JSX, SVGProps } from "react";
 import Link from "next/link";
 import "github-markdown-css";
 import ReactMarkdown from "react-markdown";
 import { parseEther } from "viem";
+import { useAccount } from "wagmi";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~~/components/ui/card";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 // Props type definition (optional but recommended for TypeScript)
+interface RepoDetail {
+  addr: string;
+  distribution_rate: number;
+  name: string;
+  url: string;
+}
+
+interface ContributorDetail {
+  addr: string;
+  distribution_rate: number;
+  name: string;
+  url: string;
+}
+interface DistributionRulesJSON {
+  contributors: Array<ContributorDetail>;
+  relatedRepos: Array<RepoDetail>;
+}
+
 interface Repo {
   title: string;
   description: string;
   ethAddress: string;
-  distributionRulesJSON: any[];
+  distributionRulesJSON: DistributionRulesJSON;
   distributionRulesMD: string;
 }
 
@@ -19,24 +39,49 @@ interface GithubShowProps {
 }
 
 export function GithubShow({ repositories }: GithubShowProps) {
+  const { address: senderAddress } = useAccount();
+  const { writeAsync, isIdle, isSuccess, isError, isLoading } = useScaffoldContractWrite({
+    contractName: "CFAv1Forwarder",
+    functionName: "createFlow",
+    args: [process.env.NEXT_PUBLIC_ROOTMUDX_TOKEN_CONTRACT, "senderAddress", "receiver", 0n, undefined],
+    value: parseEther("0"),
+    onBlockConfirmation: txnReceipt => {
+      console.log("📦 Transaction blockHash", txnReceipt.blockHash);
+    },
+  });
   // Assuming a function `createSuperfluidStream` exists to handle the stream creation
-  const createSuperfluidStream = (address, distributionRulesJSON) => {
+  const createSuperfluidStream = (address: string, distributionRulesJSON: DistributionRulesJSON) => {
     console.log(`Creating superfluid stream for address: ${address}`);
     console.log(`Creating superfluid stream based on rules: ${JSON.stringify(distributionRulesJSON)}`);
     // TODO: Here, you would typically call your API or perform the action to create the stream
     // TODO: call createFlow multiple times.
     // set flowRate by the rules. total_flowRate = a * their flowRate + b * their flowRate + c * their flowRate
-  };
-
-  const createFlow = (address, flowRate) => {
-    const { writeAsync, isLoading } = useScaffoldContractWrite({
-      contractName: "CFAv1Forwarder",
-      functionName: "createFlow",
-      args: ["sender", "receiver", "flowrate", "0x0"],
-      value: parseEther("0"),
-      onBlockConfirmation: txnReceipt => {
-        console.log("📦 Transaction blockHash", txnReceipt.blockHash);
-      },
+    const flowRateMap = new Map<string, bigint>();
+    distributionRulesJSON.contributors.forEach(contributor => {
+      if (flowRateMap.has(contributor.addr)) {
+        const flowRate = flowRateMap.get(contributor.addr);
+        if (flowRate) {
+          flowRateMap.set(contributor.addr, flowRate + BigInt(contributor.distribution_rate));
+        }
+      } else {
+        flowRateMap.set(contributor.addr, BigInt(contributor.distribution_rate));
+      }
+    });
+    distributionRulesJSON.relatedRepos.forEach(repo => {
+      if (flowRateMap.has(repo.addr)) {
+        const flowRate = flowRateMap.get(repo.addr);
+        if (flowRate) {
+          flowRateMap.set(repo.addr, flowRate + BigInt(repo.distribution_rate));
+        }
+      } else {
+        flowRateMap.set(repo.addr, BigInt(repo.distribution_rate));
+      }
+    });
+    flowRateMap.forEach((flowRate, receiver) => {
+      writeAsync({
+        args: [process.env.NEXT_PUBLIC_ROOTMUDX_TOKEN_CONTRACT, senderAddress, receiver, flowRate, undefined],
+        value: parseEther("0"),
+      });
     });
   };
 
@@ -80,7 +125,7 @@ export function GithubShow({ repositories }: GithubShowProps) {
   );
 }
 
-function EthereumIcon(props) {
+function EthereumIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
   return (
     <svg {...props} viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
       <path d="M311.9 199.9l-143.9-84c-2.5-1.5-5.7-1.5-8.3 0l-144 84c-3 1.8-3.9 5.8-2.1 8.8s5.8 3.9 8.8 2.1l135.2-79.1 135.2 79.1c1.4.8 3 1.3 4.6 1.3 2.3 0 4.5-1 6.1-2.9 1.8-3 1-7-2-8.8zM16 227.6l136 79.5v158.7c0 2.7 1.4 5.2 3.8 6.5 1.3 .8 2.7 1.1 4.1 1.1 1.7 0 3.3-.5 4.7-1.4l127.9-75.2v-149.7l-136-79.5c-3-1.8-7-.7-8.8 2.2-1.8 3-.7 7 2.2 8.8l119.6 70V371.1l-111.9 65.7V297.2c0-2.7-1.4-5.2-3.8-6.5-2.4-1.3-5.4-1.1-7.6 .5l-119.6 70V236.3c-.1-2.6-1.5-5-3.9-6.4-2.4-1.4-5.4-1.4-7.9 .2z" />
