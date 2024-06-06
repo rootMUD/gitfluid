c7add4e6a4be7f6339e6f9f1bc7a8ee48563c0f0
Yangfan <chenyangfan3211@gmail.com>
2024-06-06 12:07:14 +0800

    🎨 add donate button

--- packages/nextjs/components/GithubShow.tsx
+++ packages/nextjs/components/GithubShow.tsx
@@ -1,12 +1,6 @@
-import { JSX, SVGProps, useState } from "react";
 import Link from "next/link";
 import { GithubSuperfluidStream } from "./GithubSuperfluidStrem";
-import "github-markdown-css";
-import ReactMarkdown from "react-markdown";
-import { formatEther, parseEther } from "viem";
-import { useAccount } from "wagmi";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~~/components/ui/card";
-import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 // Props type definition (optional but recommended for TypeScript)
 interface RepoDetail {
@@ -53,34 +47,15 @@ export function GithubShow({ repositories }: GithubShowProps) {
               View
             </Link>
           </CardHeader>
-          <CardContent>
-            <div className="space-y-5">
-              <div className="flex items-center space-x-2">
-                <EthereumIcon className="w-5 h-5 text-gray-500 dark:text-gray-400" />
-                <p className="text-sm text-gray-500 dark:text-gray-400 break-all">{repo.ethAddress}</p>
-              </div>
-              <div className="space-y-1">
-                <h4 className="text-sm font-medium text-center">Distribution Rules</h4>
-                <ReactMarkdown className="space-y-1 text-sm text-gray-500 markdown-body dark:text-gray-400">
-                  {repo.distributionRulesMD}
-                </ReactMarkdown>
-              </div>
-              <GithubSuperfluidStream
-                repoAddress={repo.ethAddress}
-                distributionRulesJSON={repo.distributionRulesJSON}
-              />
-            </div>
+          <CardContent className="p-5 pt-0">
+            <GithubSuperfluidStream
+              repoAddress={repo.ethAddress}
+              distributionRulesJSON={repo.distributionRulesJSON}
+              distributionRulesMD={repo.distributionRulesMD}
+            />
           </CardContent>
         </Card>
       ))}
     </section>
-  );
-}
-
-function EthereumIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg {...props} viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-      <path d="M311.9 199.9l-143.9-84c-2.5-1.5-5.7-1.5-8.3 0l-144 84c-3 1.8-3.9 5.8-2.1 8.8s5.8 3.9 8.8 2.1l135.2-79.1 135.2 79.1c1.4.8 3 1.3 4.6 1.3 2.3 0 4.5-1 6.1-2.9 1.8-3 1-7-2-8.8zM16 227.6l136 79.5v158.7c0 2.7 1.4 5.2 3.8 6.5 1.3 .8 2.7 1.1 4.1 1.1 1.7 0 3.3-.5 4.7-1.4l127.9-75.2v-149.7l-136-79.5c-3-1.8-7-.7-8.8 2.2-1.8 3-.7 7 2.2 8.8l119.6 70V371.1l-111.9 65.7V297.2c0-2.7-1.4-5.2-3.8-6.5-2.4-1.3-5.4-1.1-7.6 .5l-119.6 70V236.3c-.1-2.6-1.5-5-3.9-6.4-2.4-1.4-5.4-1.4-7.9 .2z" />
-    </svg>
   );
 }
